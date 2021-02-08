387a191f4954923e303a689a906b3366aa1d54e4
LaptopKing <csordaspeter2002@gmail.com>
2021-02-08 11:35:54 +0100

    Program.cs fájl konfigurálva

--- Program.cs
+++ Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HetfoCsordas
 {
     class Program
     {
+        public static int hossz = File.ReadAllLines(@"erdo.txt").Count();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] erdo = Beolvas();
         }
+
+
+        // Fájl beolvasása
+        public static int[,] Beolvas()
+        {
+            int[,] erdo = new int[(hossz - 1), 10];
+
+
+
+            return erdo;
+        }
+
+        public static void Elso()
+        {
+
+        }
+
+        public static void Masodik()
+        {
+
+        }
+
+        public static void Negyedik()
+        {
+
+        }
+
+        public static void Otodik()
+        {
+
+        }
+
+        public static void Hatodik()
+        {
+
+        }
+
+        public static void Hetedik()
+        {
+
+        }
+
+        public static void Nyolcadik()
+        {
+
+        }
+
     }
 }
